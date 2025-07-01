0493c19379ef96b1b9a68281be9259790af6eaed
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-06-30 21:31:58 -0400

    Starting backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const db = new sqlite3.Database('./passwords.db', (err) => {
+	if (err) {
+		return console.error('Database connection error', err.message);
+	}
+	console.log('Connected to Database')
+});
