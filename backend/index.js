fc7c63495b68402ae4267638cd5db369c09d04c6
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-07-17 18:23:43 -0400

    Rearranged html

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -13,4 +13,31 @@ const db = new sqlite3.Database('./passwords.db', (err) => {
 		return console.error('Database connection error', err.message);
 	}
 	console.log('Connected to Database')
+});
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+	<h1> Password Manager </h1>
+	<h2> Keeping your passwords secure </h2>
+	<input type="text" id="Email" placeholder="Enter your email"><br><br>
+	<input type="password" id="Password" placeholder="Enter your password"><br>
+        <button onclick="login()">Save</button>
+
+	<script>
+		function login() {
+			const user = document.getElementById('Email').value;
+			const pass = document.getElementById('Password').value;
+			console.log('Email:', user);
+			console.log('Password:', pass);
+		}
+	</script>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}');
 });
