83e7642346ec89ffb80e2eaefb0d67d6451a3515
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-07-29 11:44:48 -0400

    Working on backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
-const express = require('express');
+honst express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.listen(PORT, (error) => {
+	if (!error) {
+		console.log(`Server is Running on port ${PORT}`);
+	} else {
+		console.log("Error, server is not running", error);
+	}
+});
 
 const db = new sqlite3.Database('./passwords.db', (err) => {
 	if (err) {
@@ -15,7 +25,18 @@ const db = new sqlite3.Database('./passwords.db', (err) => {
 	console.log('Connected to Database')
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.post('/', (req, res) => {
+	const {name} = req.body;
+	res.send(`Welcome ${name}`);
 });
+
+db.run(`
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT UNQIUE NOT NULL,
+		password TEXT NOT NULL
+	)
+`);
+
+
+
