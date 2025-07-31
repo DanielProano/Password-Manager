9bbc826d7d652482ddfd08b8c250ca15e0f46d70
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-07-31 14:23:52 -0400

    Added password adding to backend

--- backend/index.js
+++ backend/index.js
@@ -25,21 +25,43 @@ const db = new sqlite3.Database('./passwords.db', (err) => {
 	console.log('Connected to Database')
 });
 
-app.post('/', (req, res) => {
-	const {name} = req.body;
-	res.send(`Welcome ${name}`);
-});
-
 db.run(`
 	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		username TEXT UNIQUE NOT NULL,
 		password TEXT NOT NULL
-	)
-`);
+	)`, err => {
+		if (err) console.error('Table Error', err);
+		else console.log('Table access successful')
+});
 
 app.get('/api/hello', (req, res) => {
 	res.send('Hello World');
 });
 
+app.post('/api/register', async (req, res) => {
+	const { user, pass } = req.body
+
+	const saltRounds = 10
+
+	try {
+		const salt = await bcrypt.genSalt(saltRounds)
+		const hash = await bcrypt.hash(pass, salt);
+		
+		db.run(
+			`INSERT INTO users(username, password) VALUES(?, ?)`, 
+			[user, hash],
+			(err) => {
+				if (err) {
+					console.log('DB Insert Error', err.message);
+					return res.status(500).json({error: 'DB Error'});
+				}
+			res.status(201).json({ message: 'User registered successfully' });
+			}
+		);
+	} catch (err) {
+		console.log("General problem with /api/register", err);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+});
 
