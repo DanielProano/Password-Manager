1d7bd2c11ce8b2a81dc4ecdf5a62804f6e4ce606
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-08-07 19:10:01 -0400

    Not Found pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,38 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Blog from './pages/Blog';
 import About from './pages/About';
+import Virus from './pages/Virus';
+import NotFound from './pages/NotFound';
 
-import SideBar from './pages/Sidebar';
+import SideBar from './pages/SideBar'; {/* For navigation */}
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
-				<Route path="/blog" element={<Blog />} />
-				<Route path="/about" element={<About />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/*" element={<MainApp />} />
 			</Routes>
 		</BrowserRouter>
 	);			
+}
+
+function MainApp() {
+	return (
+		<div className="sidebar">
+			<SideBar />
+			<div className="content">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/virus" element={<Virus />} />
+					<Route path="/*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
