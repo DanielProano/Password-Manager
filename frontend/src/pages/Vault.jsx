5a4d9304218585b68e87661f548526ad511d09df
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-08-11 16:14:44 -0400

    testing adding info to vault

--- frontend/src/pages/Vault.jsx
+++ frontend/src/pages/Vault.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
 import { use_auth } from '../Contexts/AuthContext';
+import { encrypt, decrypt } from '../Contexts/Encrypt';
 
 function Vault() {	
 	const { derived_key, set_derived_key } = use_auth();
 	const [output, set_output] = useState('');
+	const [showPopup, setShowPopup] = useState(false);
+
+	const [service, setService] = useState('');
+	const [login, setLogin] = useState('');
+	const [password, setPassword] = useState('');
+	const [notes, setNotes] = useState('');
 
 	async function GetInfo() {
 		try {
 			if (derived_key) {
 				const { key, token } = derived_key;
-
-				console.log(key);
-				console.log(token);
 
 		 		const response = await fetch('/api/vault/get', {
         				method: 'GET',
@@ -29,13 +33,50 @@ function Vault() {
 					console.log('Failed to get Passwords');
 				}
 			} else {
-				set_output('Key does not exist');
+				set_output('Unauthorized');
 			}
 		} catch (error) {
 			console.log('Error:', error);
 			set_output(error);
 		}
 	}
+
+	async function AddInfo() {
+		try {
+			if (derived_key) {
+				const { key, token } = derived_key;
+
+				const { iv: service_iv, data: service_data } = await encrypt(key, service);
+				const { iv: login_iv, data: login_data } = await encrypt(key, login);
+				const {iv: pass_iv, data: pass_data } = await encrypt(key, password);
+				const {iv: notes_iv, data: notes_data } = await encrypt(key, notes);
+
+				const response = await fetch('/api/vault/store', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${token}`},
+					body: JSON.stringify({
+						service: { iv: service_iv, data: service_data },
+						login: { iv: login_iv, data: login_data },
+						password: { iv: pass_iv, data: pass_data },
+						notes: { iv: notes_iv, data: notes_data }
+					})
+				});
+
+				if (response.ok) {
+					const data = await response.json();
+					console.log('Added info');
+					GetInfo();
+				} else {
+					console.log('Failed to add info');
+				}
+			}
+		} catch (error) {
+			console.log("Error", error);
+		}
+	} 
+
 
 	useEffect(() => {
 		GetInfo();
@@ -50,9 +91,24 @@ function Vault() {
 	      <input placeholder='Search' /><br /><br />
 	    </div>
 	    <button>Search></button>
+
 	    <div class="Vault-Add-Button">
-	      <button>Add</button>
+	      <button onClick={() => setShowPopup(true)}>Add</button>
 	    </div>
+
+	    {showPopup && (
+	      <div className="popup-setup">
+	        <div className="popup">
+	          <h2>Add New Password</h2>
+	          <button onClick={() => setShowPopup(false)}>X</button>
+	          <input placeholder="Service" value={service} onChange={(e) => setService(e.target.value)} /><br />
+	          <input placeholder="Login" value={login} onChange={(e) => setLogin(e.target.value)} /><br />
+	          <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
+	          <textarea placeholder="Notes" value={notes} onChange={(e) => setNotes(e.target.value)} ></textarea><br />
+	          <button onClick={() => setShowPopup(false); AddInfo(); }>Done</button>
+	        </div>
+	      </div>
+	    )}
    	  </div>
   	);
 };
